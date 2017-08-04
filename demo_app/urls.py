292cbde4aac0ebb4fc8c5e8c3ff6d3c8d453aaea
saarthak10 <saarthaksharma87@gmail.com>
2017-08-05 01:02:18 +0530

    url for login,signup,feed etc

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'',views.signup_view),
-    url(r'^register/success/',views.register_success)
+    url(r'post/',views.post_view),
+    url(r'feed/',views.feed_view),
+    url(r'like',views.like_view),
+    url(r'comment',views.comment_view),
+    url(r'login/',views.login_view, name= 'login'),
+    url(r'', views.signup_view)
 
 
 ]
